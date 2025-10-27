2d840e39a685a926222a4001f187a88f815bc722
Bryan Ng <ncwbryan@gmail.com>
2025-10-27 14:11:51 +0900

    dp: 3725

---
+++ Dynamic Programming/3725. Count Ways to Choose Coprime Integers from Rows.py
@@ -0,0 +1,14 @@
+from functools import cache
+from math import gcd
+from typing import List
+
+class Solution:
+    def countCoprime(self, mat: List[List[int]]) -> int:
+        MOD = 1_000_000_007
+
+        @cache
+        def dfs(i, g):
+            if i < 0:
+                return 1 if g == 1 else 0
+            return sum(dfs(i-1, gcd(g, x)) for x in mat[i]) % MOD
+        return dfs(len(mat)-1, 0)
